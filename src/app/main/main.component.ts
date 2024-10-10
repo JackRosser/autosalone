4cd8349a8154fd614602272483b29d83ec0c00c2
Giacomo Rossettini <rossegiac@gmail.com>
2024-10-11 00:22:29 +0200

    carousel

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements AfterViewInit {
+  @ViewChild('cardList') cardList!: ElementRef;
+  currentIndex = 0;
 
+  ngAfterViewInit() {
+    this.updateCarousel();
+  }
+
+  updateCarousel() {
+    const cardWidth = this.cardList.nativeElement.querySelector('.card-item').clientWidth;
+    this.cardList.nativeElement.style.transform = `translateX(-${this.currentIndex * cardWidth}px)`;
+  }
+
+  prevSlide() {
+    const totalCards = this.cardList.nativeElement.querySelectorAll('.card-item').length;
+    this.currentIndex = (this.currentIndex - 1 + totalCards) % totalCards;
+    this.updateCarousel();
+  }
+
+  nextSlide() {
+    const totalCards = this.cardList.nativeElement.querySelectorAll('.card-item').length;
+    this.currentIndex = (this.currentIndex + 1) % totalCards;
+    this.updateCarousel();
+  }
 }
